0d939fb14afcf4a893e15a837ae26a186b5b5368
Christian Kovanen <christian@intor.fi>
2024-08-17 18:25:43 +0300

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -559,14 +559,25 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      autotag = { enable = false },
+      autopairs = { enable = true },
+      endwise = { enable = true },
     },
     config = function(_, opts)
       require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = true,
+        },
+      }
     end,
     dependencies = {
       'nvim-treesitter/nvim-treesitter-context',
+      'windwp/nvim-ts-autotag',
     },
   },
 }, {
